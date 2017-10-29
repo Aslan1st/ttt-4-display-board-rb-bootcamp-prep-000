6bcded71803178db3aabe39e54a698c5cbf93f5d
Aslan1st <aslan1st@mac.com>
2017-10-29 03:30:24 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,10 +1,10 @@
 $board = Array.new(9, " ")
 
 def display_board(board = $board)
-   board_game_devision = "-----------"
+   board_game_division = "-----------"
    puts " #{board[0]} | #{board[1]} | #{board[2]} ",
-        board_game_devision,
+        board_game_division,
         " #{board[3]} | #{board[4]} | #{board[5]} ",
-        board_game_devision,
+        board_game_division,
         " #{board[6]} | #{board[7]} | #{board[8]} "
 end
